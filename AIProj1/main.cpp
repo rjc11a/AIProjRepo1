1bdac0591413b5293b82f811f3b0631224051aa8
rjc11a <Joseph@Josephs-MacBook-Pro.local>
2017-02-18 18:38:12 -0600

    Initial Commit

---
+++ AIProj1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AIProj1
+//
+//  Created by Joseph on 2/18/17.
+//  Copyright © 2017 JosephC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
